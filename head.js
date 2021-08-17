84c991a41899d6dc07f146b91a8111c0971a1001
Gagandeep Uppal <guppal.profile@gmail.com>
2021-08-17 02:36:31 +0000

    added test case for array head

---
+++ head.js
@@ -0,0 +1,21 @@
+let assertEqual = function(actual, expected) {
+  if ( actual === expected ){
+   // console.log("👍Assertion Passed: [" + actual + "] === [" + expected + "]");
+console.log(`Assertion passed: ${actual} === ${expected}`)
+  } else {
+  console.log("🛑Assertion Failed: [" + actual + "] !== [" + expected + "]");
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+//return the first element or undefined if array is empty
+let head = function(array){
+return array[0];
+};
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]),undefined);
