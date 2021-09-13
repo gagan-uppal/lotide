c9690b2e03952eb25065ff14bc92dd9fea9a05cb
Gagandeep Uppal <guppal.profile@gmail.com>
2021-09-13 20:53:01 +0000

    testcases added for retrieving only head

--- head.js
+++ head.js
@@ -1,5 +1,5 @@
 let assertEqual = function(actual, expected) {
-  if ( actual === expected ){
+  if (actual === expected) {
    // console.log("👍Assertion Passed: [" + actual + "] === [" + expected + "]");
 console.log(`Assertion passed: ${actual} === ${expected}`)
   } else {
@@ -19,3 +19,4 @@ return array[0];
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]),undefined);
+assertEqual(head([1]), 1);
