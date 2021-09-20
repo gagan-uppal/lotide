7f2bc349c254f70040ebf3066d00a0b43edbefb6
Gagandeep Uppal <guppal.profile@gmail.com>
2021-09-20 18:13:35 +0000

    test files updated, exported

---
+++ test/assertEqualTest.js
@@ -0,0 +1,13 @@
+
+// test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+// ...
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Gagan", "Gagan");
+assertEqual(1, 3);
